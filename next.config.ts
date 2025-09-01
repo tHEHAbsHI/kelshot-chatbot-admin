7959af2a1441b8bfde9717b53c8c7393f830ac3b
Abdullah <saleemwork331@gmail.com>
2025-09-01 19:41:18 +0500

    Fixed cors issue

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/v1/:path*',
-        destination: `${process.env.BACKEND_URL || 'http://ec2-54-91-102-216.compute-1.amazonaws.com/api/v1'}/:path*/`,
+        destination: `${process.env.BACKEND_URL || 'http://ec2-54-91-102-216.compute-1.amazonaws.com/api/v1'}/:path*`,
       },
     ];
   },
