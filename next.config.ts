0019d10156f4a6f718eee39a82910c8fd230ae40
Abdullah <saleemwork331@gmail.com>
2025-09-02 17:48:12 +0500

    Changed backend url

--- next.config.ts
+++ next.config.ts
@@ -6,19 +6,19 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/v1/tasks',
-        destination: `${process.env.BACKEND_URL || 'http://ec2-54-91-102-216.compute-1.amazonaws.com/api/v1'}/tasks/`,
+        destination: `${process.env.BACKEND_URL || 'http://ec2-54-226-183-152.compute-1.amazonaws.com/api/v1'}/tasks/`,
       },
       {
         source: '/api/v1/users',
-        destination: `${process.env.BACKEND_URL || 'http://ec2-54-91-102-216.compute-1.amazonaws.com/api/v1'}/users/`,
+        destination: `${process.env.BACKEND_URL || 'http://ec2-54-226-183-152.compute-1.amazonaws.com/api/v1'}/users/`,
       },
       {
         source: '/api/v1/conversations',
-        destination: `${process.env.BACKEND_URL || 'http://ec2-54-91-102-216.compute-1.amazonaws.com/api/v1'}/conversations/`,
+        destination: `${process.env.BACKEND_URL || 'http://ec2-54-226-183-152.compute-1.amazonaws.com/api/v1'}/conversations/`,
       },
       {
         source: '/api/v1/:path*',
-        destination: `${process.env.BACKEND_URL || 'http://ec2-54-91-102-216.compute-1.amazonaws.com/api/v1'}/:path*`,
+        destination: `${process.env.BACKEND_URL || 'http://ec2-54-226-183-152.compute-1.amazonaws.com/api/v1'}/:path*`,
       },
     ];
   },
