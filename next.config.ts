70aea8dbb948d02df6aaf84682a8e9143f508d5f
Abdullah <saleemwork331@gmail.com>
2025-09-10 04:15:13 +0500

    Added conversations and notes

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,10 @@ const nextConfig: NextConfig = {
         destination: `${process.env.BACKEND_URL || 'http://ec2-54-90-93-250.compute-1.amazonaws.com/api/v1'}/conversations/`,
       },
       {
+        source: '/api/v1/notes',
+        destination: `${process.env.BACKEND_URL || 'http://ec2-54-90-93-250.compute-1.amazonaws.com/api/v1'}/notes/`,
+      },
+      {
         source: '/api/v1/:path*',
         destination: `${process.env.BACKEND_URL || 'http://ec2-54-90-93-250.compute-1.amazonaws.com/api/v1'}/:path*`,
       },
